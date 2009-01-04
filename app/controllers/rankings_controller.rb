ec6e481d9335527910fdfb4e601b74c8e9623600
John Dewey <john@redbubble.local>
2009-01-04 00:22:58 -0800

    setup basic view

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,6 @@
 class RankingsController < ApplicationController
   def index
-    @stuff = Timeline.find_profanity_by_user_id(params['user_id'])
+    profanity = Timeline.find_profanity_by_user_id(params['user_id'])
+    @rating = profanity.rate if profanity
   end
 end
---
+++ config/initializers/register_javascript.rb
@@ -0,0 +1,1 @@
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :common => %w(jquery-ui jquery jrails)
